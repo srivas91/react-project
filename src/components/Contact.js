beb1b5713149c244c5e14b25ee1adba77767fe61
unknown <srini.pit21@gmail.com>
2021-06-13 22:37:21 +0530

    latest

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,9 @@ function Contact()
     return (
         <div className="mycontact">
         <h4>Contact Form</h4>
-        <input type="text" name="name" placeholder="Name"></input><br/>
-        <input type="email" name="name" placeholder="Email Id"></input><br/>
-        <textarea rows="5" cols="60"></textarea>
+        <input type="text" name="name" placeholder="Name"></input><br/><br/>
+        <input type="email" name="name" placeholder="Email Id"></input><br/><br/>
+        <textarea rows="5" cols="60" placeholder="Message"></textarea>
         </div>
     );
 }
